c4cf93f3be44e0a455e902481aa14022bda7a3aa
Sahil goyal <s.g.navingoyal@gmail.com>
2020-09-01 00:09:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import paramiko
 paramiko.SSHClient().set_missing_host_key_policy(paramiko.AutoAddPolicy())
 paramiko.SSHClient().connect("192.168.1.5",22, "sahil", "1234")
 stdin, stdout, stderr = ssh.exec_command("ls")
