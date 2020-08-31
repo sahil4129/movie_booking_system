62b3f344788c071aeafe0d8fa2d57dfac7d75bba
Sahil goyal <s.g.navingoyal@gmail.com>
2020-08-31 23:46:30 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,15 @@
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x is 1.")
+pip install paramiko
+host = "192.168.1.5"
+port = 22
+username = "sahil"
+password = "1234"
+
+command = "ls"
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
+
+stdin, stdout, stderr = ssh.exec_command(command)
+lines = stdout.readlines()
+print(lines)
